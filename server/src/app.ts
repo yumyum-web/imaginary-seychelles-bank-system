cf72b6ba9a4f9c888ac3128e4551db60612a4b06
Yumeth <yumethsumathipala@gmail.com>
2024-10-25 02:57:25 +0530

    Add error handling middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,7 @@ import compression from "compression";
 import v1ApiDoc from "./api/v1/api-doc.js";
 import { joinPaths, makeDirIfNotExists } from "./utils.js";
 import { paths } from "./config.js";
+import { ExpressError } from "./api/v1/errors.js";
 
 const pathsDir = joinPaths(paths.srcDir, "api", "v1", "paths");
 makeDirIfNotExists(pathsDir);
@@ -26,5 +27,18 @@ await initialize({
   dependencies: {},
   paths: pathsDir,
 });
+
+app.use(
+  (
+    err: ExpressError,
+    _req: express.Request,
+    res: express.Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _next: express.NextFunction,
+  ) => {
+    res.status(err.status || 500);
+    res.send(err.message);
+  },
+);
 
 export default app;
