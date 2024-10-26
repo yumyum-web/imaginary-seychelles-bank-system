82fdcf95ab9996d09dccdac92d93511196461b11
Yumeth <yumethsumathipala@gmail.com>
2024-10-26 15:52:08 +0530

    Add database helper

--- server/src/api/config.ts
+++ server/src/api/config.ts
@@ -13,6 +13,11 @@ const optionalEnvVar = (name: string, defaultValue: string): string => {
 };
 
 export const config = {
+  dbHost: requiredEnvVar("DB_HOST"),
+  dbPort: parseInt(optionalEnvVar("DB_PORT", "3306")),
+  dbUser: requiredEnvVar("DB_USER"),
+  dbPassword: requiredEnvVar("DB_PASSWORD"),
+  dbName: requiredEnvVar("DB_NAME"),
   jwtTokenSecret: requiredEnvVar("JWT_TOKEN_SECRET"),
   jwtTokenExpiresIn: optionalEnvVar("JWT_TOKEN_EXPIRES_IN", "1h"),
 };
