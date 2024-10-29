b99b6f8a05d6c08d1987626ac4540a5cfafa287d
Yumeth <yumethsumathipala@gmail.com>
2024-10-29 13:27:54 +0530

    Improve `account/transfer` bad requests responses

--- server/src/api/v1/handlers/account/transfer.ts
+++ server/src/api/v1/handlers/account/transfer.ts
@@ -1,6 +1,6 @@
 import { Handler } from "openapi-backend";
 import conn from "../../helpers/db.js";
-import { ProcedureCallPacket, RowDataPacket } from "mysql2";
+import { ProcedureCallPacket, RowDataPacket, QueryError } from "mysql2";
 import { User } from "../../models/User.js";
 
 interface TransferBody {
@@ -13,6 +13,16 @@ const accountTransfer: Handler<TransferBody> = async (c, _, res) => {
   const { fromAccountId, toAccountId, amount } = c.request.requestBody;
   const user: User = c.security.jwt;
 
+  if (fromAccountId === toAccountId) {
+    return res
+      .status(400)
+      .json({ message: "Cannot transfer to the same account." });
+  }
+
+  if (amount <= 0) {
+    return res.status(400).json({ message: "Invalid amount." });
+  }
+
   try {
     const query = `
             CALL Online_Transfer(?, ?, ?, ?);
@@ -24,6 +34,22 @@ const accountTransfer: Handler<TransferBody> = async (c, _, res) => {
     console.log("Transfer result:", result);
     return res.status(200).json({ message: "Transfer successful." });
   } catch (error) {
+    if ((error as QueryError).code === "ER_NO_REFERENCED_ROW_2") {
+      return res.status(400).json({ message: "Invalid to account ID." });
+    } else if (
+      (error as QueryError).message === "You are not the owner of the account"
+    ) {
+      return res
+        .status(400)
+        .json({ message: "You are not the owner of the account." });
+    } else if (
+      (error as QueryError).message ===
+      "Amount exceeds the maximum withdrawal limit."
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Insufficient balance in the account." });
+    }
     console.error("Failed to transfer:", error);
     return res.status(500).json({ message: "Failed to transfer." });
   }
